39f0560a692c0e9e101a68342e4794af1a83f9c0
liuweiwei <505831001@qq.com>
2020-12-24 00:22:39 +0800

    SpringBoot整合Spring Security安全框架Demo01

--- springboot-security/src/main/java/com/liuweiwei/controller/TbLoginController.java
+++ springboot-security/src/main/java/com/liuweiwei/controller/TbLoginController.java
@@ -4,10 +4,18 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.liuweiwei.model.TbToken;
 import com.liuweiwei.security.JwtAuthenticatioToken;
-import com.liuweiwei.utils.SecurityUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author liuweiwei
@@ -22,6 +33,12 @@ import java.io.IOException;
  */
 @RestController
 public class TbLoginController {
+
+    private static final String USERNAME = Claims.SUBJECT;
+    private static final String CREATED = "created";
+    private static final String AUTHORITIES = "authorities";
+    private static final String SECRET = "abcdefgh";
+    private static final long EXPIRE_TIME = 12 * 60 * 60 * 1000;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -36,7 +53,25 @@ public class TbLoginController {
         /**
          * 系统登录认证
          */
-        JwtAuthenticatioToken token = SecurityUtils.login(request, username, password, authenticationManager);
+        JwtAuthenticatioToken token = new JwtAuthenticatioToken(username, password);
+        WebAuthenticationDetailsSource detailsSource = new WebAuthenticationDetailsSource();
+        WebAuthenticationDetails buildDetails = detailsSource.buildDetails(request);
+        token.setDetails(buildDetails);
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = authenticationManager.authenticate(token);
+        context.setAuthentication(authentication);
+
+        Map<String, Object> claims = new HashMap<>(3);
+        claims.put(USERNAME, ((UserDetails) authentication.getPrincipal()).getUsername());
+        claims.put(CREATED, new Date());
+        claims.put(AUTHORITIES, authentication.getAuthorities());
+
+        Date expirationDate = new Date(System.currentTimeMillis() + EXPIRE_TIME);
+        String generateToken = Jwts.builder().setClaims(claims).setExpiration(expirationDate).signWith(SignatureAlgorithm.HS512, SECRET).compact();
+
+        token.setToken(generateToken);
+
         TbToken tbToken = JSON.parseObject(JSON.toJSONString(token), new TypeReference<TbToken>() {});
         return tbToken;
     }
--- springboot-security/src/main/java/com/liuweiwei/security/JwtLoginFilter.java
+++ springboot-security/src/main/java/com/liuweiwei/security/JwtLoginFilter.java
@@ -2,13 +2,15 @@ package com.liuweiwei.security;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.liuweiwei.utils.HttpUtils;
-import com.liuweiwei.utils.JwtTokenUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -22,6 +24,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 启动登录认证流程过滤器
@@ -29,6 +34,12 @@ import java.nio.charset.Charset;
  * @since 2020-05-20
  */
 public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
+
+	private static final String USERNAME = Claims.SUBJECT;
+	private static final String CREATED = "created";
+	private static final String AUTHORITIES = "authorities";
+	private static final String SECRET = "abcdefgh";
+	private static final long EXPIRE_TIME = 12 * 60 * 60 * 1000;
 	
 	public JwtLoginFilter(AuthenticationManager authManager) {
         setAuthenticationManager(authManager);
@@ -85,8 +96,40 @@ public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
 			eventPublisher.publishEvent(new InteractiveAuthenticationSuccessEvent(authResult, this.getClass()));
 		}
 		// 生成并返回token给客户端，后续访问携带此token
-		JwtAuthenticatioToken token = new JwtAuthenticatioToken(null, null, JwtTokenUtils.generateToken(authResult));
-		HttpUtils.write(response, token);
+		JwtAuthenticatioToken token = new JwtAuthenticatioToken(null, null, generateToken(authResult));
+		write(response, token);
+	}
+
+	public static String generateToken(Authentication authentication) {
+		Map<String, Object> claims = new HashMap<>(3);
+		claims.put(USERNAME, getUsername(authentication));
+		claims.put(CREATED, new Date());
+		claims.put(AUTHORITIES, authentication.getAuthorities());
+		return generateToken(claims);
+	}
+
+	public static String getUsername(Authentication authentication) {
+		String username = null;
+		if(authentication != null) {
+			Object principal = authentication.getPrincipal();
+			if(principal != null && principal instanceof UserDetails) {
+				username = ((UserDetails) principal).getUsername();
+			}
+		}
+		return username;
+	}
+
+	private static String generateToken(Map<String, Object> claims) {
+		Date expirationDate = new Date(System.currentTimeMillis() + EXPIRE_TIME);
+		return Jwts.builder().setClaims(claims).setExpiration(expirationDate).signWith(SignatureAlgorithm.HS512, SECRET).compact();
+	}
+
+	public static void write(HttpServletResponse response, Object data) throws IOException {
+		response.setContentType("application/json; charset=utf-8");
+		String json = JSONObject.toJSONString(data);
+		response.getWriter().print(json);
+		response.getWriter().flush();
+		response.getWriter().close();
 	}
 	
 	/** 
