2bd9ffbf0eeda600b7462b4a1108bf8d44cc4c57
liuweiwei <505831001@qq.com>
2021-02-27 11:09:28 +0800

    SpringBoot框架：SpringApplication，ConfigurableApplicationContext，ConfigurableEnvironment。

--- springboot-druid/src/main/java/org/liuweiwei/DemoDruidApplication.java
+++ springboot-druid/src/main/java/org/liuweiwei/DemoDruidApplication.java
@@ -48,19 +48,19 @@ public class DemoDruidApplication {
     private static final Logger logger = LogManager.getLogger(DemoDruidApplication.class);
 
     /**
-     * Class that can be used to bootstrap and launch a Spring application from a Java main method.
-     * By default class will perform the following steps to bootstrap your application:
+     * 类，可以用来从Java主方法引导和启动Spring应用程序。
+     * 默认情况下，class将执行以下步骤来引导应用程序：
      */
     private static SpringApplication application;
     /**
-     * SPI interface to be implemented by most if not all application contexts.
-     * Provides facilities to configure an application context in addition to the application context client methods in the {ApplicationContext} interface.
+     * 大部分(如果不是全部)应用程序上下文都要实现的SPI接口。
+     * 除了{ApplicationContext}接口中的应用程序上下文客户端方法之外，还提供了配置应用程序上下文的工具。
      */
     private static ConfigurableApplicationContext applicationContext;
     /**
-     * Configuration interface to be implemented by most if not all {Environment} types.
-     * Provides facilities for setting active and default profiles and manipulating underlying property sources.
-     * Allows clients to set and validate required properties, customize the conversion service and more through the {ConfigurablePropertyResolver} superinterface.
+     * 大多数(如果不是全部){Environment}类型都要实现的配置接口。
+     * 提供用于设置活动和默认概要文件以及操作底层属性源的工具。
+     * 允许客户端通过{ConfigurablePropertyResolver}超接口设置和验证所需的属性，自定义转换服务和更多。
      */
     private static ConfigurableEnvironment applicationContextEnvironment;
 
