7a6eec6a0180635bce29330c3e56007197fe8d9e
72137497 <72137497@vivo.com>
2021-08-14 09:24:36 +0800

    2021-8-14 MyBatisPlus多数据源＋AESUtils工具类加密

---
+++ springboot-swagger2/src/test/java/org/liuweiwei/AESUtilsTest.java
@@ -0,0 +1,26 @@
+package org.liuweiwei;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.liuweiwei.utils.AESUtils;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AESUtilsTest {
+
+    /**
+     * org.apache.ibatis.binding.BindingException: Invalid bound statement (not found):
+     */
+    @Test
+    public void contextLoads() {
+        /**固定 16 位长度的加密串*/
+        String key = "23asdwee2343afwe";
+        /**被加密的密文内容*/
+        String content = "12345678";
+        String enS = AESUtils.encrypt(key, content);
+        System.out.println("原始加密：" + enS);
+        System.out.println("原始解密：" + AESUtils.decrypt(key, enS));
+    }
+}
