6741a68b4906f8c47d3dbd96729920c1ab0ec10c
72137497 <72137497@vivo.com>
2021-08-24 09:33:26 +0800

    2021-8-24 SpringBoot整合SpringSecurity＋OpenAuthority2技术

--- springboot-security-jwt/src/main/java/org/liuweiwei/utils/JwtTools.java
+++ springboot-security-jwt/src/main/java/org/liuweiwei/utils/JwtTools.java
@@ -1,7 +1,18 @@
 package org.liuweiwei.utils;
 
+import com.alibaba.fastjson.JSONObject;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author Liuweiwei
@@ -10,5 +21,64 @@ import org.springframework.stereotype.Component;
 @Component
 @Log4j2
 public class JwtTools {
-    
+    private static final String JWT_PAYLOAD_USER_KEY = "user";
+
+    /**
+     * 私钥加密token
+     *
+     * @param userInfo   载荷中的数据
+     * @param privateKey 私钥
+     * @param expire     过期时间，单位分钟
+     * @return JWT
+     */
+    public static String generateTokenExpireInMinutes(Object userInfo, PrivateKey privateKey, int expire) {
+        return Jwts.builder()
+                .claim(JWT_PAYLOAD_USER_KEY, JSONObject.toJSONString(userInfo))
+                .setId(new String(Base64.getEncoder().encode(UUID.randomUUID().toString().getBytes())))
+                .setExpiration(new Date(System.currentTimeMillis() + expire))
+                .signWith(SignatureAlgorithm.RS256, privateKey)
+                .compact();
+    }
+
+    /**
+     * 私钥加密token
+     *
+     * @param userInfo   载荷中的数据
+     * @param privateKey 私钥
+     * @param expire     过期时间，单位秒
+     * @return JWT
+     */
+    public static String generateTokenExpireInSeconds(Object userInfo, PrivateKey privateKey, int expire) {
+        return Jwts.builder()
+                .claim(JWT_PAYLOAD_USER_KEY, JSONObject.toJSONString(userInfo))
+                .setId(new String(Base64.getEncoder().encode(UUID.randomUUID().toString().getBytes())))
+                .setExpiration(new Date(System.currentTimeMillis() + expire))
+                .signWith(SignatureAlgorithm.RS256, privateKey)
+                .compact();
+    }
+
+    /**
+     * 公钥解析token
+     *
+     * @param token     用户请求中的token
+     * @param publicKey 公钥
+     * @return Jws<Claims>
+     */
+    private static Jws<Claims> parserToken(String token, PublicKey publicKey) {
+        return Jwts.parser().setSigningKey(publicKey).parseClaimsJws(token);
+    }
+
+    /**
+     * 获取token中的载荷信息
+     * 获取token中的用户信息
+     *
+     * @param token     用户请求中的令牌
+     * @param publicKey 公钥
+     * @return 用户信息
+     */
+    public static Claims getInfoFromToken(String token, PublicKey publicKey) {
+        Jws<Claims> claimsJws = parserToken(token, publicKey);
+        Claims claims = claimsJws.getBody();
+        return claims;
+    }
 }
