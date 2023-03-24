3e7daf37e2d3a32df45f9c9222da8f0ba515ffcf
LiuWeiWei <505831001@163.com>
2023-03-24 10:01:44 +0800

    SpringBoot整合ThymeLeaf区分Resources和Webapp两种静态资源配置实现

--- springboot-thymeleaf/src/main/java/com/example/DemoThymeLeafApplication.java
+++ springboot-thymeleaf/src/main/java/com/example/DemoThymeLeafApplication.java
@@ -8,6 +8,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
+ * 1.SpringBoot静态资源存放于Spring-Boot-AutoConfigure-2.2.4.Release.jar
+ * 2.ResourceProperties类中有个全量字符数组定义String[] CLASSPATH_RESOURCE_LOCATIONS=
+ * 3.资源字符数组={"classpath:/META-INF/resources/", "classpath:/resources/", "classpath:/static/", "classpath:/public/"};
+ * 4.1.静态资源核心配置方式一：spring.resources.static-locations=classpath:/resources/, classpath:/static/, classpath:/public/
+ * 4.2.静态资源核心配置方式二：spring.mvc.static-path-pattern=/static/**
+ * 4.1.此配置适用于WebApp目录下
+ * 4.2.此配置适用于Resources目录下
+ *
  * @author Liuweiwei
  * @since 2021-01-08
  */
--- springboot-thymeleaf/src/main/java/com/example/controller/HtmlPageController.java
+++ springboot-thymeleaf/src/main/java/com/example/controller/HtmlPageController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HtmlPageController {
 
+    /**
+     * 取决于配置：spring.mvc.static-path-pattern: /static/**
+     * D:\workspace-idea\springboot-demo\springboot-thymeleaf\src\main\resources\templates\index.html
+     * 取决于配置：spring.resources.static-locations: classpath:/resources/, classpath:/static/, classpath:/public/
+     * D:\workspace-idea\springboot-demo\springboot-thymeleaf\src\main\webapp\templates\index.html
+     * @return
+     */
     @GetMapping(value = "/index")
     public String index() {
         return "index";
